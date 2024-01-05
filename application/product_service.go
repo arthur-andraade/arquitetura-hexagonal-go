69921c54f79d1e4c1ec6cf9c59cf419f228a0342
Arthur Andrade <arthurandradenf@gmail.com>
2024-01-05 11:10:39 -0300

    feat: Implement method Create and its tests

--- application/product_service.go
+++ application/product_service.go
@@ -14,3 +14,24 @@ func (s *ProductService) Get(id string) (ProductInterface, error) {
 
 	return product, nil
 }
+
+func (s *ProductService) Create(name string, price float64) (ProductInterface, error) {
+
+	newProduct := NewProduct()
+	newProduct.Name = name
+	newProduct.Price = price
+
+	_, err := newProduct.IsValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	productSaved, err := s.Persistence.Save(newProduct)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return productSaved, nil
+}
--- application/product_service_test.go
+++ application/product_service_test.go
@@ -51,3 +51,63 @@ func TestProductService_Get(t *testing.T) {
 
 	defer controlador.Finish()
 }
+
+func TestProductService_Create(t *testing.T) {
+
+	controlador := gomock.NewController(t)
+
+	productInterfaceMock := mock_application.NewMockProductInterface(controlador)
+
+	t.Run("Price invalid", func(t *testing.T) {
+
+		persistenceMock := mock_application.NewMockProductPersistenceInterface(controlador)
+
+		persistenceMock.EXPECT().Save(gomock.Any()).Times(0)
+
+		service := application.ProductService{
+			Persistence: persistenceMock,
+		}
+
+		productReturned, err := service.Create("Laptop", -500)
+
+		require.Nil(t, productReturned)
+		require.NotNil(t, err)
+		require.Equal(t, "the price must be greater or equal zero", err.Error())
+
+	})
+
+	t.Run("Return product created and saved", func(t *testing.T) {
+
+		persistenceMock := mock_application.NewMockProductPersistenceInterface(controlador)
+
+		persistenceMock.EXPECT().Save(gomock.Any()).Return(productInterfaceMock, nil).AnyTimes()
+
+		service := application.ProductService{
+			Persistence: persistenceMock,
+		}
+
+		productReturned, err := service.Create("Laptop", 5000)
+
+		require.Nil(t, err)
+		require.NotNil(t, productReturned)
+
+	})
+
+	t.Run("Return erro when try save the new product", func(t *testing.T) {
+
+		persistenceMock := mock_application.NewMockProductPersistenceInterface(controlador)
+
+		persistenceMock.EXPECT().Save(gomock.Any()).Return(nil, errors.New("Erro")).AnyTimes()
+
+		service := application.ProductService{
+			Persistence: persistenceMock,
+		}
+
+		productReturned, err := service.Create("Laptop", 5000)
+
+		require.NotNil(t, err)
+		require.Nil(t, productReturned)
+	})
+
+	defer controlador.Finish()
+}
